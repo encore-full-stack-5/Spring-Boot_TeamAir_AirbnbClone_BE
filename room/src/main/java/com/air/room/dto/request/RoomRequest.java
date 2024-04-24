ac7e4a6ed305eaef1837bd087c066b00a20cd4be
Sujin1517 <11tnwls12@gmail.com>
2024-04-24 09:16:28 +0900

    update room update dto method

--- room/src/main/java/com/air/room/dto/request/RoomRequest.java
+++ room/src/main/java/com/air/room/dto/request/RoomRequest.java
@@ -8,6 +8,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record RoomRequest(
         //userid는 토큰으로
@@ -93,134 +94,98 @@ public record RoomRequest(
     }
 
     public List<RoomAccessibility> getRoomAccessibilityList(Room room){
-        List<RoomAccessibility> roomAccessibilityList = new ArrayList<>();
-        for(Integer id : this.accessibility()){
-            RoomAccessibility roomAccessibility = RoomAccessibility.builder()
-                    .room(room)
-                    .accessibility(Accessibility.builder()
-                            .id(id)
-                            .build())
-                    .build();
-            roomAccessibilityList.add(roomAccessibility);
-        }
-        return roomAccessibilityList;
+        return Arrays.stream(this.accessibility).map(id ->
+                        RoomAccessibility.builder()
+                                .room(room)
+                                .accessibility(Accessibility.builder()
+                                        .id(id)
+                                        .build())
+                                .build())
+                .toList();
     }
 
     public List<RoomAmenity> getRoomAmenityList(Room room){
-        List<RoomAmenity> roomAmenityList = new ArrayList<>();
-        for(Integer id : this.amenities()){
-            RoomAmenity roomAmenity = RoomAmenity.builder()
-                    .room(room)
-                    .amenity(Amenity.builder()
-                            .id(id)
-                            .build())
-                    .build();
-            roomAmenityList.add(roomAmenity);
-        }
-        return roomAmenityList;
+        return Arrays.stream(this.amenities).map(id ->
+                        RoomAmenity.builder()
+                                .room(room)
+                                .amenity(Amenity.builder()
+                                        .id(id)
+                                        .build())
+                                .build())
+                .toList();
     }
 
     public List<RoomUniqueAmenity> getRoomUniqueAmenityList(Room room){
-        List<RoomUniqueAmenity> roomUniqueAmenityList = new ArrayList<>();
-        for(Integer id : this.uniqueAmenities()){
-            RoomUniqueAmenity roomUniqueAmenity = RoomUniqueAmenity.builder()
-                    .room(room)
-                    .uniqueAmenity(UniqueAmenity.builder()
+        return Arrays.stream(this.uniqueAmenities).map(id ->
+                RoomUniqueAmenity.builder()
+                        .room(room)
+                        .uniqueAmenity(UniqueAmenity.builder()
                             .id(id)
                             .build())
-                    .build();
-            roomUniqueAmenityList.add(roomUniqueAmenity);
-        }
-        return roomUniqueAmenityList;
+                        .build())
+                .toList();
     }
 
     public List<Integer> getDeleteRoomAmenityByIdList(Room room) {
-        List<Integer> deleteRoomAmenityIdList = new ArrayList<>();
         List<Integer> amenityIdList = new ArrayList<>(Arrays.asList(this.amenities));
-        room.getRoomAmenities().stream().map(e -> {
-            Integer id = e.getAmenity().getId();
-            if(!amenityIdList.contains(id)){
-                deleteRoomAmenityIdList.add(id);
-            }
-            return null;
-        });
-        return deleteRoomAmenityIdList;
+        return room.getRoomAmenities().stream()
+                .map(e -> e.getAmenity().getId())
+                .filter(id -> !amenityIdList.contains(id))
+                .toList();
     }
     public List<RoomAmenity> getAddRoomAmenityList(Room room) {
-        List<RoomAmenity> addRoomAmenityIdList = new ArrayList<>();
         List<Integer> amenityIdList =
                 room.getRoomAmenities().stream().map(e -> e.getAmenity().getId()).toList();
-        Arrays.stream(this.amenities).map(e -> {
-            if(!amenityIdList.contains(e)) {
-                addRoomAmenityIdList.add(RoomAmenity.builder()
+        return Arrays.stream(this.amenities)
+                .filter(e -> !amenityIdList.contains(e))
+                .map(id -> RoomAmenity.builder()
                         .room(room)
                         .amenity(Amenity.builder()
-                                .id(e)
+                                .id(id)
                                 .build())
-                        .build());
-            }
-            return null;
-        });
-        return addRoomAmenityIdList;
+                        .build())
+                .toList();
     }
 
     public List<Integer> getDeleteRoomUniqueAmenityByIdList(Room room) {
-        List<Integer> deleteRoomUniqueAmenityIdList = new ArrayList<>();
         List<Integer> UniqueAmenityIdList = new ArrayList<>(Arrays.asList(this.uniqueAmenities));
-        room.getRoomUniqueAmenities().stream().map(e -> {
-            Integer id = e.getUniqueAmenity().getId();
-            if(!UniqueAmenityIdList.contains(id)){
-                deleteRoomUniqueAmenityIdList.add(id);
-            }
-            return null;
-        });
-        return deleteRoomUniqueAmenityIdList;
+        return room.getRoomUniqueAmenities().stream()
+                .map(e -> e.getUniqueAmenity().getId())
+                .filter(id -> !UniqueAmenityIdList.contains(id))
+                .toList();
     }
     public List<RoomUniqueAmenity> getAddRoomUniqueAmenityList(Room room) {
-        List<RoomUniqueAmenity> addRoomUniqueAmenityIdList = new ArrayList<>();
         List<Integer> UniqueAmenityIdList =
                 room.getRoomUniqueAmenities().stream().map(e -> e.getUniqueAmenity().getId()).toList();
-        Arrays.stream(this.uniqueAmenities).map(e -> {
-            if(!UniqueAmenityIdList.contains(e)) {
-                addRoomUniqueAmenityIdList.add(RoomUniqueAmenity.builder()
+        return Arrays.stream(this.uniqueAmenities)
+                .filter(e -> !UniqueAmenityIdList.contains(e))
+                .map(id -> RoomUniqueAmenity.builder()
                         .room(room)
                         .uniqueAmenity(UniqueAmenity.builder()
-                                .id(e)
+                                .id(id)
                                 .build())
-                        .build());
-            }
-            return null;
-        });
-        return addRoomUniqueAmenityIdList;
+                        .build())
+                .toList();
     }
 
     public List<Integer> getDeleteRoomAccessibilityByIdList(Room room) {
-        List<Integer> deleteRoomAccessibilityIdList = new ArrayList<>();
         List<Integer> accessibilityIdList = new ArrayList<>(Arrays.asList(this.accessibility));
-        room.getRoomAccessibility().stream().map(e -> {
-            Integer id = e.getAccessibility().getId();
-            if(!accessibilityIdList.contains(id)){
-                deleteRoomAccessibilityIdList.add(id);
-            }
-            return null;
-        });
-        return deleteRoomAccessibilityIdList;
+        return room.getRoomAccessibility().stream()
+                .map(e -> e.getAccessibility().getId())
+                .filter(id -> !accessibilityIdList.contains(id))
+                .toList();
     }
     public List<RoomAccessibility> getAddRoomAccessibilityList(Room room) {
-        List<RoomAccessibility> addRoomAccessibilityIdList = new ArrayList<>();
         List<Integer> accessibilityIdList =
                 room.getRoomAccessibility().stream().map(e -> e.getAccessibility().getId()).toList();
-        Arrays.stream(this.accessibility).map(e -> {
-            if(!accessibilityIdList.contains(e)) {
-                addRoomAccessibilityIdList.add(RoomAccessibility.builder()
+        return Arrays.stream(this.accessibility)
+                .filter(e -> !accessibilityIdList.contains(e))
+                .map(id -> RoomAccessibility.builder()
                         .room(room)
                         .accessibility(Accessibility.builder()
-                                .id(e)
+                                .id(id)
                                 .build())
-                        .build());
-            }
-            return null;
-        });
-        return addRoomAccessibilityIdList;
+                        .build())
+                .toList();
     }
 }
