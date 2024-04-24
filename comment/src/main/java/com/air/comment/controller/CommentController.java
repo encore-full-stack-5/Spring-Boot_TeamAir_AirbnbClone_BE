7b024460553150ed685f20ae3177f1cd41f79039
jaeyoon00 <qwero55@naver.com>
2024-04-24 15:51:31 +0900

    wishlist final

--- comment/src/main/java/com/air/comment/controller/CommentController.java
+++ comment/src/main/java/com/air/comment/controller/CommentController.java
@@ -1,4 +1,15 @@
 package com.air.comment.controller;
 
+import com.air.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/comment")
+@RequiredArgsConstructor
 public class CommentController {
+    private final CommentService commentService;
+
+
 }
